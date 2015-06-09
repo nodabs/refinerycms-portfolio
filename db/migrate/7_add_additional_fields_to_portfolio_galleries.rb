81c1220df6c65b1ed73a217ea7ffaed9491a5049
Nick Romanowski <nick@paweblabs.com>
2015-06-09 13:50:08 -0400

    changed migration name

--- db/migrate/7_add_additional_fields_to_portfolio_galleries.rb
+++ db/migrate/7_add_additional_fields_to_portfolio_galleries.rb
@@ -1,4 +1,4 @@
-class RemoveCoverImageIdFromGalleries < ActiveRecord::Migration
+class AddAdditionalFieldsToGalleries < ActiveRecord::Migration
 
   add_column :refinery_portfolio_galleries, :featured_homepage, :boolean, :default => false
   add_column :refinery_portfolio_galleries, :featured_utilities, :boolean, :default => false
