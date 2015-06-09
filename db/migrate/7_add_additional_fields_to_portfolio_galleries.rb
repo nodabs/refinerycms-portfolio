46f1c1716fe6a9f2a70a73fa84e9810266fb0d1c
Nick Romanowski <nick@paweblabs.com>
2015-06-09 13:46:57 -0400

    need to remove year from migration because it was already in the db table

--- db/migrate/7_add_additional_fields_to_portfolio_galleries.rb
+++ db/migrate/7_add_additional_fields_to_portfolio_galleries.rb
@@ -1,6 +1,5 @@
 class RemoveCoverImageIdFromGalleries < ActiveRecord::Migration
 
-  add_column :refinery_portfolio_galleries, :year, :integer, :default => 2012
   add_column :refinery_portfolio_galleries, :featured_homepage, :boolean, :default => false
   add_column :refinery_portfolio_galleries, :featured_utilities, :boolean, :default => false
   add_column :refinery_portfolio_galleries, :featured_commercial, :boolean, :default => false
