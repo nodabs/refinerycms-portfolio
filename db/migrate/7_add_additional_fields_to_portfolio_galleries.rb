3a7c0c6ddae687072e031af4d5ad6f7878d41696
Nick Romanowski <nick@paweblabs.com>
2015-06-09 13:20:46 -0400

    additional fields

---
+++ db/migrate/7_add_additional_fields_to_portfolio_galleries.rb
@@ -0,0 +1,10 @@
+class RemoveCoverImageIdFromGalleries < ActiveRecord::Migration
+
+  add_column :refinery_portfolio_galleries, :year, :integer, :default => 2012
+  add_column :refinery_portfolio_galleries, :featured_homepage, :boolean, :default => false
+  add_column :refinery_portfolio_galleries, :featured_utilities, :boolean, :default => false
+  add_column :refinery_portfolio_galleries, :featured_commercial, :boolean, :default => false
+  add_column :refinery_portfolio_galleries, :featured_agricultural, :boolean, :default => false
+  add_column :refinery_portfolio_galleries, :featured_residential, :boolean, :default => false
+
+end
